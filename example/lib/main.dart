cc193aa6cd763967ebd4207cb2188dbb8ea2acd5
Hedeve01 <nazari.fph@gmail.com>
2024-06-25 12:34:11 +0330

    refactor: chnage example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:example/screens/home_page.dart';
+import 'package:auto_type/auto_type.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,5 +33,45 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: const HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AutoType autoType = AutoType();
+  String displayText = '';
+  @override
+  void initState() {
+    super.initState();
+    autoType.typeWriter(
+      repeat: false,
+      textsCharacter: ["Hello Every One"],
+      updateCallback: (p0) {
+        setState(() {
+          displayText = p0;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(displayText, style: const TextStyle(fontSize: 16)),
+          )
+        ],
+      )),
+    );
   }
 }
